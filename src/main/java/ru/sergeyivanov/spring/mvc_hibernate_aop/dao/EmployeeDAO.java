05f2a61b5142b2d86910b010a3c617f9839872ec
Sergey Ivanov <ivanovsergey.samara@gmail.com>
2022-10-01 11:49:44 +0400

    commit syn

--- src/main/java/ru/sergeyivanov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
+++ src/main/java/ru/sergeyivanov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -5,7 +5,6 @@ import ru.sergeyivanov.spring.mvc_hibernate_aop.entity.Employee;
 import java.util.List;
 
 public interface EmployeeDAO {
-
     public List<Employee> getAllEmployees();
 
     public void saveEmployee(Employee employee);
